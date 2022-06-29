b0fb759950a4ea443d8bcb8ea1711b0d194bd143
Igor Lukjanov <lukjanov.igr@gmail.com>
2022-06-29 12:51:51 +0300

    Add hamburger

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,48 @@
 import React, { useState } from 'react';
 import './Navbar.scss';
 import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <nav className="navbar">
-      <NavLink to="/" onClick={() => setIsOpen(false)}>
-        Home
-      </NavLink>
+      <div className="navbar-center">
+        <div className="navbar__header">
+          <img src={logo} alt="logo" className="logo" />
+          <button
+            class={`fa-solid fa-bars nav-toggle ${isOpen ? 'nav-toggle-animate' : ''}`}
+            onClick={() => setIsOpen(!isOpen)}
+          ></button>
+        </div>
 
-      <div className="toggle-button" onClick={() => setIsOpen(!isOpen)}>
-        <span className={`bar bar-1 ${isOpen && 'open'}`}></span>
-        <span className={`bar bar-2 ${isOpen && 'open'}`}></span>
-        <span className={`bar bar-3 ${isOpen && 'open'}`}></span>
-      </div>
-
-      {isOpen && (
-        <ul className="mobile-menu">
-          <NavLink to="/about" onClick={() => setIsOpen(false)}>
+        <ul className={`navbar__links ${isOpen ? 'show-links' : ''}`}>
+          <NavLink to="/" onClick={() => setIsOpen(!isOpen)}>
+            Home
+          </NavLink>
+          <NavLink to="/about" onClick={() => setIsOpen(!isOpen)}>
             About
           </NavLink>
-          <NavLink to="/projects" onClick={() => setIsOpen(false)}>
+          <NavLink to="/projects" onClick={() => setIsOpen(!isOpen)}>
             Projects
           </NavLink>
-          <NavLink to="/skills" onClick={() => setIsOpen(false)}>
+          <NavLink to="/skills" onClick={() => setIsOpen(!isOpen)}>
             Skills
           </NavLink>
-          <NavLink to="/contact" onClick={() => setIsOpen(false)}>
+          <NavLink to="/contact" onClick={() => setIsOpen(!isOpen)}>
             Contact
           </NavLink>
         </ul>
-      )}
 
-      <ul className="navbar__links">
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/skills">Skills</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </ul>
+        <ul className="navbar__socials">
+          <a href="https://www.twitter.com" class="fab fa-facebook"></a>
+          <a href="https://www.twitter.com" class="fab fa-twitter"></a>
+          <a href="https://www.twitter.com" class="fab fa-behance"></a>
+          <a href="https://www.twitter.com" class="fab fa-linkedin"></a>
+          <a href="https://www.twitter.com" class="fab fa-sketch"></a>
+        </ul>
+      </div>
     </nav>
   );
 }
